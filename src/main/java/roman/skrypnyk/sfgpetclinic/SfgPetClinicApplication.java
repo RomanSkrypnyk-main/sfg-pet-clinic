1465eeac0adc59ecf24382c58b1fcf03129bdc87
Roman Skrypnyk <62649514+RomanSkrypnyk-main@users.noreply.github.com>
2022-04-12 12:51:58 +0300

    Initial commit

---
+++ src/main/java/roman/skrypnyk/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package roman.skrypnyk.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
