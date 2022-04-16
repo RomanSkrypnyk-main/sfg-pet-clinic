a01d43541249450ea6d60fb9d15e1553f6271a39
Roman Skrypnyk <62649514+RomanSkrypnyk-main@users.noreply.github.com>
2022-04-16 14:49:28 +0300

    add BaseEntity.

---
+++ pet-clinic-data/src/main/java/roman/skrypnyk/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,14 @@
+package roman.skrypnyk.sfgpetclinic.model;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/roman/skrypnyk/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/roman/skrypnyk/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package roman.skrypnyk.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
