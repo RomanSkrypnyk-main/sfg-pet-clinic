fbae73605fd26eea67456be3195fd96ec9352c26
Roman Skrypnyk <62649514+RomanSkrypnyk-main@users.noreply.github.com>
2022-05-07 17:53:39 +0300

    Impl Map based Services.

---
+++ pet-clinic-data/src/main/java/roman/skrypnyk/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package roman.skrypnyk.sfgpetclinic.services.map;
+
+import roman.skrypnyk.sfgpetclinic.model.Pet;
+import roman.skrypnyk.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
