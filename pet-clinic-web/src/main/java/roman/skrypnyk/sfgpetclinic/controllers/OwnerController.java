d21bfad653bf5b491601e2cc14dd38c31f0f44d0
Roman Skrypnyk <62649514+RomanSkrypnyk-main@users.noreply.github.com>
2022-05-19 20:49:35 +0300

    add OwnerList & update controller.

--- pet-clinic-web/src/main/java/roman/skrypnyk/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/roman/skrypnyk/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package roman.skrypnyk.sfgpetclinic.controllers;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import roman.skrypnyk.sfgpetclinic.services.OwnerService;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
 
+	private OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"", "/", "/owners/index"})
-	public String listOwners(){
+	public String listOwners(@NotNull Model model){
+
+	model.addAttribute("owners", ownerService.findAll());
+
 		return "owners/index";
 	}
 
